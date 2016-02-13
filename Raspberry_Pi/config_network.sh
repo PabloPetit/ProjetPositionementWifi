796f87c4fdb005220e97e1ef6541a8da6b4103b1
PabloPetit <pablopetit94@gmail.com>
2016-02-13 15:47:25 +0100

    maj

--- Raspberry_Pi/config_network.sh
+++ Raspberry_Pi/config_network.sh
@@ -2,5 +2,7 @@
 
 ifconfig wlan0 up
 iwconfig wlan0 mode ad-hoc
-iwconfig wlan0 essid ""
-iwconfig wlan0 inet 192.162.1.42
+iwconfig wlan0 essid "ESP"
+iwconfig wlan0 channel 3
+iwconfig wlan0 key s:password
+iwconfig wlan0 inet 192.162.1.22
